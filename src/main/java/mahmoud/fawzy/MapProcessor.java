7e2158efe3520bdea9d39c0cbfde52dcf5695c2c
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-10-07 16:27:35 +0200

    Improve MapProcessor implementation

--- src/main/java/mahmoud/fawzy/MapProcessor.java
+++ src/main/java/mahmoud/fawzy/MapProcessor.java
@@ -5,14 +5,17 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.Objects;
 import java.util.function.Function;
 
-public class MapProcessor<IN, OUT> implements Processor<IN, OUT> {
+public class MapProcessor<IN, OUT> implements Processor<IN, OUT>, Subscription {
 
 
     private final Publisher<? extends IN> upstreamPublisher;
     private final Function<IN, OUT> mapper;
     private Subscriber<? super OUT> downstreamSubscriber;
+    private boolean terminated;
+    private Subscription subscriptionToUpstreamPublisher;
 
 
     public MapProcessor(Publisher<? extends IN> upstreamPublisher, Function<IN, OUT> mapper) {
@@ -33,24 +36,58 @@ public class MapProcessor<IN, OUT> implements Processor<IN, OUT> {
     public void onSubscribe(Subscription subscription) {
         // Forward signal to downstream Subscriber, then the downstream subscriber will start to request(n), which is propagated through the subscription object
         // Then the upstream Publisher will call onNext() on its Subscriber, which is this Processor
-        downstreamSubscriber.onSubscribe(subscription);
+        this.subscriptionToUpstreamPublisher = subscription;
+        downstreamSubscriber.onSubscribe(this);
     }
 
     @Override
     public void onNext(IN in) {
+
         // Transform signal and forward it to downstream Subscriber
-        downstreamSubscriber.onNext(mapper.apply(in));
+
+        if (terminated) return; // Protection against asynchronous cancellation
+
+        OUT apply;
+        try {
+            Objects.requireNonNull(apply = mapper.apply(in));
+        } catch (Throwable t) {
+            cancel(); // Cancel subscription upstream, cancellation is asynchronous potentially
+            onError(t); // Signal error downstream
+            return;
+        }
+
+        downstreamSubscriber.onNext(apply);
     }
 
     @Override
     public void onError(Throwable throwable) {
+        if (terminated) return;
+
         // Forward signal to downstream Subscriber
+        this.terminated = true;
         downstreamSubscriber.onError(throwable);
     }
 
     @Override
     public void onComplete() {
+        if (terminated) return;
+
         // Forward signal to downstream Subscriber
+        this.terminated = true;
         downstreamSubscriber.onComplete();
+    }
+
+    // Make ourselves a subscription in order to avoid creating a new subscription object
+    @Override
+    public void request(long l) {
+        if (terminated) return;
+
+        this.subscriptionToUpstreamPublisher.request(l);
+    }
+
+    @Override
+    public void cancel() {
+
+        this.subscriptionToUpstreamPublisher.cancel();
     }
 }
